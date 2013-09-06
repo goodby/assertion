d1ed04768aaff34ff01a91c4854f4f151b28400f
suin <suinyeze@gmail.com>
2013-09-06 14:07:20 +0900

    add new assertions

--- src/Goodby/Assertion/Assert.php
+++ src/Goodby/Assertion/Assert.php
@@ -40,13 +40,52 @@ class Assert
     public static function argumentLengthAtLeast($value, $minimum, $message = null)
     {
         if (is_array($value)) {
-            $length = count($value) < $minimum;
+            $length = count($value);
         } else {
             $length = strlen($value);
         }
 
         if ($length < $minimum) {
             throw new InvalidArgumentException($message);
+        }
+    }
+
+    /**
+     * @param callable $function
+     * @param string $message
+     * @throws InvalidArgumentException
+     */
+    public static function argumentObligates(callable $function, $message = null)
+    {
+        if ($function() === true) {
+            return;
+        }
+
+        throw new InvalidArgumentException($message);
+    }
+
+    /**
+     * @param string $class
+     * @param string $message
+     * @throws InvalidArgumentException
+     */
+    public static function argumentClassExists($class, $message = null)
+    {
+        if (class_exists($class) === false) {
+            throw new InvalidArgumentException(str_replace('%class%', $class, $message));
+        }
+    }
+
+    /**
+     * @param string $class
+     * @param string $parent
+     * @param string $message
+     * @throws InvalidArgumentException
+     */
+    public static function argumentSubclassOf($class, $parent, $message = null)
+    {
+        if (is_subclass_of($class, $parent) === false) {
+            throw new InvalidArgumentException(str_replace('%parent%', $parent, $message));
         }
     }
 }
